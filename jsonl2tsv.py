4f30f9709589ba3a0b9523d5e86fdc1d0e9f1a9a
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-09-02 15:46:02 +0300

    Update jsonl2tsv.py

--- jsonl2tsv.py
+++ jsonl2tsv.py
@@ -5,6 +5,7 @@
 import sys
 import json
 
+from datetime import datetime
 from argparse import ArgumentParser
 from logging import warning
 
@@ -20,18 +21,21 @@ def normalize_space(text):
     return text.replace('\t', ' ').replace('\n', ' ')
 
 
-def output(id_, text, user, accepted, fn, ln):
+def output(id_, text, user, created, accepted, fn, ln):
     try:
         id_ = normalize_space(id_)
         text = normalize_space(text)
         if user is None:
             user = str(user)
         user = normalize_space(user)
+        if created is None:
+            created = str(created)
+        created = normalize_space(created)
         if not accepted:
             accepted = None
         else:
             accepted = ','.join(accepted)
-        print('{}\t{}\t{}\t{}'.format(id_, user, accepted, text))
+        print('{}\t{}\t{}\t{}\t{}'.format(id_, user, created, accepted, text))
     except:
         raise ValueError('{} line {}: {} {} {}'.format(
             fn, ln, id_, text, accepted))
@@ -51,6 +55,17 @@ def get_annotator(data, options):
         return session
 
 
+def get_created(data, date_only=False):
+    created = data.get('created')
+    if created is None:
+        return None
+    elif not date_only:
+        return created
+    else:
+        created = datetime.fromisoformat(created)    # Python 3.7
+        return str(created.date())
+
+
 def process(fn, options):
     with open(fn) as f:
         for ln, l in enumerate(f, start=1):
@@ -64,7 +79,8 @@ def process(fn, options):
             text = data['text']
             accepted = data['accept']
             annotator = get_annotator(data, options)
-            output(id_, text, annotator, accepted, fn, ln)
+            created = get_created(data)
+            output(id_, text, annotator, created, accepted, fn, ln)
 
 
 def main(argv):
